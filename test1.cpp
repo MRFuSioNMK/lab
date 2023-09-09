25e5b22bc10014a952fec8d82b748ab60685db5c
MRFuSioNMK <144415774+MRFuSioNMK@users.noreply.github.com>
2023-09-09 20:08:37 +0200

    Add files via upload

--- test1.cpp
+++ test1.cpp
@@ -1,13 +1,12 @@
 #include <iostream> //i/o library
 using namespace std; //instead od prefix std::
-
 int main()//header
 {  //begin
     float a, res; //declaration input
-    cout <<"a = ";
-    cin >> a;
-    res = a + 5;
-    cout <<" a + 5 = " <<res <<endl;
-    system ("Pause");
+    cout <<"a = ";//view text to console
+    cin >> a;//enter changes from the console
+    res = a + 5;//calculate the result
+    cout <<" a+5 = " <<res <<endl;//display the result to the console4
+    
     return 0; //return to OS
 }  // end
